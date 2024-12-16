6c4e5366943b9f4e6fc07f96cae7b0cf3daa86ee
Gilbert Elikplim Kukah <kwamegilbert1114@gmail.com>
2024-12-16 07:41:22 +0000

    2024-12-16 07:41:20

--- commit.bat
+++ commit.bat
@@ -5,5 +5,6 @@ set commit_message=%dt:~0,4%-%dt:~4,2%-%dt:~6,2% %dt:~8,2%:%dt:~10,2%:%dt:~12,2%
 git add -A
 git commit --allow-empty -m "%commit_message%"
 git push
+echo "Done Commiting"
 timeout /t 120 > nul
 goto loop
